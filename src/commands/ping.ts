17da26621a09afce751034df4160df986a423306
unknown <marcosfassiob18@vt.edu>
2023-01-07 16:01:39 -0500

    update ping command to include response time

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,18 @@
 import { inlineCode, SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 import { Command } from "../structures/Command";
+import { CommandType } from "../util/CommandType";
 
 export = new Command({
-    data: new SlashCommandBuilder()
+    type: CommandType.Information,
+    builder: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('pong.'),
-    run: async function(interaction: CommandInteraction) {
-        await interaction.reply('pong!');
+    run: async function(interaction: ChatInputCommandInteraction) {
+        let time = new Date().valueOf();
+        await interaction.reply("pong!").then(() => {
+            time = new Date().valueOf() - time;
+            interaction.editReply(`pong! ${inlineCode(`${time} ms`)}`)
+        })
     }
 })
